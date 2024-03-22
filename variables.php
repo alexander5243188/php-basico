f613e7eb659a347765aa75bf48bf63a704296502
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-03-21 20:09:04 -0400

    Declaracion de variable y constante

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+
+// Declaración de variables
+    $variable_numero_uno = 3;
+    $variable_numero_dos = 4;
+
+    echo $variable_numero_uno + $variable_numero_dos;    
+    echo "\n";
+
+// Declaracion de  constantes, estas deben ir siempre en mayusculas
+define("NOMBRE", "Alexander");
+define("EDAD", 44);
+
+echo NOMBRE ." ". EDAD;
+?>
