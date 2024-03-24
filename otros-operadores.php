276d615b23a478ab5ac48a68bd8c1cedb3ddae13
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-03-24 10:56:56 -0400

    otros operadores

---
+++ otros-operadores.php
@@ -0,0 +1,46 @@
+<?php 
+//------------------------ Asignacion
+$primera_cadena = "Alexander";
+var_dump($primera_cadena);
+
+//------------------------ Incremento
+$primer_valor_numerico = 4;
+echo "$primer_valor_numerico += 6 =";
+$primer_valor_numerico += 6;
+var_dump($primer_valor_numerico);
+
+$segundo_valor_numerico = 2;
+echo "$segundo_valor_numerico ++ = ";
+$segundo_valor_numerico ++;
+var_dump($segundo_valor_numerico);
+
+//------------------------ Decremento
+$tercer_valor_numerico = 5;
+echo "$tercer_valor_numerico -= 2 = ";
+$tercer_valor_numerico -= 2;
+var_dump($tercer_valor_numerico);
+
+$cuarto_valor_numerico = 6;
+echo "$cuarto_valor_numerico -- = ";
+$cuarto_valor_numerico --;
+var_dump($cuarto_valor_numerico);
+
+//------------------------ Multiplicación
+$quinto_valor_numerico = 8;
+echo "$quinto_valor_numerico *= 2 = ";
+$quinto_valor_numerico *= 2;
+var_dump($quinto_valor_numerico);
+
+//------------------------  División
+$sexto_valor_numerico = 10;
+echo "$sexto_valor_numerico /= 2 = ";
+$sexto_valor_numerico /= 2;
+var_dump($sexto_valor_numerico);
+
+//------------------------ Cocatenacion
+$primera_cadena_texto = "Hola";
+$primera_cadena_texto .= " "."Mundo";
+echo($primera_cadena_texto);
+
+//------------------------ 
+?>
